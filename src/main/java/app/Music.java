15a72631de265416abf018fe8ae6dd5f31369f9b
Matheus Sabadin <matheushs15@hotmail.com>
2023-03-26 20:10:52 -0300

    Implementar o backend que transforma textos em sons

--- src/main/java/app/Music.java
+++ src/main/java/app/Music.java
@@ -1,33 +1,101 @@
 package app;
 
-import java.util.ArrayList;
 import org.jfugue.pattern.Pattern;
+import org.jfugue.player.Player;
+
+import java.util.ArrayList;
 
 public class Music {
     // Constants
     private final int defaultVolumeValue = 50;
     private final int defaultBPMValue = 120;
     private final int defaultOctaveValue = 5;
+    private final int defaultInstrument = 0;
+    private final int defaultOctave = 5;
+
 
     // Properties
-    private ArrayList<Pattern> sequentialPatterns;
+    private ArrayList<PatternHandler> sequentialPatternHandlers;
     private PatternHandler actualPatternBuilder;
 
 
     // Constructor
-    public Music(){
-        this.sequentialPatterns = new ArrayList<Pattern>();
-        this.actualPatternBuilder = new PatternHandler(defaultVolumeValue, defaultBPMValue, PatternHandler.Instrument.GUITAR, defaultOctaveValue);
+    public Music(ArrayList<SegmentAction> musicActions) throws Exception {
+        this.sequentialPatternHandlers = initializePatternHandlerList();
+        actualPatternBuilder = this.sequentialPatternHandlers.get(0);
+        generatePatternHandlers(musicActions);
     }
 
 
     // Methods
-    public void playText(String rawText){
+    private ArrayList<PatternHandler> initializePatternHandlerList(){
+        ArrayList<PatternHandler> patternHandlerList = new ArrayList<PatternHandler>();
+        patternHandlerList.add(generateFirstPatternHandler());
 
-        for (int i = 0; i < rawText.length(); i++) {
-            SegmentAction segmentAction = TextSegmentToActionMapper.checkSegmentAction(rawText, i);
+        return patternHandlerList;
+    }
+    private PatternHandler generateFirstPatternHandler(){
+        return new PatternHandler(defaultVolumeValue, defaultBPMValue, defaultInstrument, defaultOctave);
+    }
+    private void generatePatternHandlers(ArrayList<SegmentAction> musicSegmentActions) throws Exception {
+        for (SegmentAction musicSegmentAction: musicSegmentActions)
+            makeActionOnPattern(musicSegmentAction);
+    }
+    private void makeActionOnPattern(SegmentAction musicSegmentAction) throws Exception {
+        if (musicSegmentAction.Type == SegmentAction.ActionType.PLAY_NOTE)
+            addNoteToMusic(musicSegmentAction);
+        else
+            changePropertyOnMusic(musicSegmentAction);
+    }
+    private void addNoteToMusic(SegmentAction musicSegmentAction) throws Exception {
+        PlayNoteAction playNoteAction = (PlayNoteAction)musicSegmentAction;
+        this.actualPatternBuilder.addNoteToPattern(playNoteAction.NoteToPlay);
+    }
+    private void changePropertyOnMusic(SegmentAction musicSegmentAction) throws Exception {
+        ChangePropertyAction changePropertyAction = (ChangePropertyAction)musicSegmentAction;
+        ChangePropertyAction.ActionOnProperty action = changePropertyAction.ActionOnProperty;
+        int actionValue = changePropertyAction.PropertyNewValue;
+
+        PatternHandler newPatternHandler = getPatternWithNewProperty(action, actionValue);
+        this.sequentialPatternHandlers.add(newPatternHandler);
+        this.actualPatternBuilder = this.sequentialPatternHandlers.get(this.sequentialPatternHandlers.size() - 1);
+    }
+    private PatternHandler getPatternWithNewProperty(ChangePropertyAction.ActionOnProperty action, int actionValue) throws Exception {
+        switch (action){
+            case SET_VALUE_TO_INSTRUMENT:
+                return new PatternHandler(actualPatternBuilder.getVolume(), actualPatternBuilder.getBPM(), actionValue, actualPatternBuilder.getOctave());
+            case RAISE_OCTAVE:
+                return new PatternHandler(actualPatternBuilder.getVolume(), actualPatternBuilder.getBPM(), actualPatternBuilder.getInstrument(), actualPatternBuilder.getOctave() + actionValue);
+            case DOUBLE_VOLUME:
+                return new PatternHandler(actualPatternBuilder.getVolume() * 2, actualPatternBuilder.getBPM(), actualPatternBuilder.getInstrument(), actualPatternBuilder.getOctave());
+            case SET_VALUE_TO_BPM:
+                return new PatternHandler(actualPatternBuilder.getVolume(), actionValue, actualPatternBuilder.getInstrument(), actualPatternBuilder.getOctave());
+            case ADD_VALUE_TO_INSTRUMENT:
+                return new PatternHandler(actualPatternBuilder.getVolume(), actualPatternBuilder.getBPM(), actualPatternBuilder.getInstrument() + actionValue, actualPatternBuilder.getOctave());
+            default:
+                throw new Exception("Erro, parâmetros inválidos");
         }
     }
+
+
+    public Pattern joinSequentialPatterns(){
+        Pattern joinedPatterns = new Pattern();
+
+        for (PatternHandler patternHandler: this.sequentialPatternHandlers) {
+            Pattern pattern = patternHandler.generatePattern();
+            joinedPatterns.add(pattern, 1);
+        }
+
+        return joinedPatterns;
+    }
+
+    public void playText() throws Exception {
+        Pattern joinedPatterns = joinSequentialPatterns();
+        new Player().play(joinedPatterns);
+    }
+
+
+
 
    
     
